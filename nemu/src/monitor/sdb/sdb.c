2cd8eccddf1d3c9be68098e0164b595f875a6b13
tracer-ics2022 <tracer@njuics.org>
2022-09-16 14:35:04 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:35:04 up 37 min,  1 user,  load average: 0.27, 0.33, 0.35

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -98,8 +98,8 @@ static int cmd_x(char *args){
 			vaddr_t addr = expr(express, &succ);
 			if(succ){
 				for(int i = 0; i < n; ++i){
-					printf("address: 0x%08x\t\tvalue: 0x%02x\n", addr, vaddr_read(addr, 1));
-					addr += 1;
+					printf("address: 0x%08x\t\tvalue: 0x%08x\n", addr, vaddr_read(addr, 4));
+					addr += 4;
 				} 
 				return 0;
 			}else{
