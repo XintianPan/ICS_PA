1ca91a5f87dbd344121058ea1853b493d19dfdc8
tracer-ics2022 <tracer@njuics.org>
2022-09-17 16:49:43 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:49:43 up  4:13,  1 user,  load average: 0.37, 0.59, 0.63

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -22,20 +22,29 @@ int is_exit_status_bad();
 word_t expr(char* e, bool* success);
 
 int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
-  FILE *fp = fopen("../nemu/tools/gen-expr/build/input", "r");
+
+	/* Initialize the monitor. */
+#ifdef CONFIG_EXPRTEST
+	FILE *fp = fopen("../nemu/tools/gen-expr/build/input", "r");
   if(fp == NULL)
 	  puts("No file!");
   else{ 
 	  word_t ret = 0;
 	  char buf[65536] = {};
 	  memset(buf, 0, sizeof buf);
-	  if(fscanf(fp, "%u %[^\n]%*c", &ret, buf) != EOF){
+	  while(fscanf(fp, "%u %[^\n]%*c", &ret, buf) != EOF){
 		  printf("%u %s\n", ret, buf);
+		  bool succ = false;
+		  word_t cmp = expr(buf, &succ);
+		  if(succ)
+			  if(cmp == ret)
+				  puts("YES");
 		  memset(buf, 0, sizeof buf);
-	  }
-	}
+ 	  }
+ 	}
   fclose(fp);
+#endif
+
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
 #else
