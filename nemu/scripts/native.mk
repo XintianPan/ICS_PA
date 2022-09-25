b1375c46f186e4e965608e49e7c2f07e59fb6c70
tracer-ics2022 <tracer@njuics.org>
2022-09-25 15:44:56 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:44:56 up 25 min,  1 user,  load average: 0.29, 0.48, 0.44

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,7 +26,6 @@ $(BINARY): compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
-override ARGS += --batch
 # Command to execute NEMU
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
