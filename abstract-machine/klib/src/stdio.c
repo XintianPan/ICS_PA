55f67048e5a09aaa1376312e8c3a527690807b82
tracer-ics2022 <tracer@njuics.org>
2022-09-28 14:09:11 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:09:11 up 13 min,  1 user,  load average: 0.50, 0.34, 0.22

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,57 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char _std_num_buf[20];
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
-}
+} 
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	char *s;
+	int d;
+	size_t j = -1;
+	size_t i = 0;
+	va_start(ap, fmt);
+	while(*fmt){
+		if(*fmt == '%'){
+			++fmt;
+			switch(*fmt){
+				case 'd':
+					d = va_arg(ap, int);
+					j = -1;
+					do{
+						_std_num_buf[++j] = d % 10 + '0';
+						d /= 10;
+					}while(d);
+					for(register int k = j; k >= 0; --k)
+						out[i++] = _std_num_buf[k];
+					break;
+				case 's':
+					s = va_arg(ap, char *);
+					while(*s){
+						out[i++] = *s;
+						++s;
+					}
+					break;
+				case '%':
+					out[i++] = *fmt;
+					break;
+				default:
+					break;
+			}
+		}else{
+			out[i++] = *fmt; 
+		}
+		++fmt;	
+	}
+	out[i] = '\0';
+	return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
