43509cbc95e87ab9420b5d8352f320906426930b
tracer-ics2022 <tracer@njuics.org>
2022-10-12 14:10:59 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:10:59 up 17 min,  1 user,  load average: 0.74, 2.77, 2.10

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,6 +19,7 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < 32; ++i){
+	printf("0x%08x, 0x%08x\n", ref_r->gpr[i], cpu.gpr[i]);
 	if(ref_r->gpr[i] != cpu.gpr[i])
 		return false;
   }
