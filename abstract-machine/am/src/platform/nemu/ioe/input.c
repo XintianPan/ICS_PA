50249e2bffbdfb8e33eb0d443f595f83f1cf116f
tracer-ics2022 <tracer@njuics.org>
2022-10-13 12:42:49 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:42:49 up 22 min,  1 user,  load average: 0.57, 0.52, 0.44

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,6 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-	kbd->keycode = inb(KBD_ADDR);
+	kbd->keycode = inl(KBD_ADDR);
 	kbd->keydown = (kbd->keycode & KEYDOWN_MASK) ? true : false;
 }
