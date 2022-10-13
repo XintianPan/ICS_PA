566784c5fda1d3faba08350adcc4086a074fd54d
tracer-ics2022 <tracer@njuics.org>
2022-10-13 13:35:22 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:35:22 up  1:15,  1 user,  load average: 0.36, 0.45, 0.36

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -34,7 +34,13 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if(size == 0)
+	  return NULL;
+  else{
+	  void *mem = heap.start;
+	  heap.start += size;
+	  return mem; 
+  }
 #endif
   return NULL;
 }
