3bf684a11146a94acbd46ba6b8ea6fad725c0f26
tracer-ics2022 <tracer@njuics.org>
2022-10-25 19:49:27 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:27 up 9 min,  1 user,  load average: 0.30, 0.27, 0.18

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -31,6 +31,7 @@ static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+	puts("here");
 }
 
 void init_audio() {
