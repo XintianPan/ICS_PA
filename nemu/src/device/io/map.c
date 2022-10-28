902cac1632fa64ec3c02909fff06b0d9932169d0
tracer-ics2022 <tracer@njuics.org>
2022-10-28 14:35:26 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:35:26 up 10 min,  1 user,  load average: 0.28, 0.38, 0.27

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,10 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+#ifdef CONFIG_DTRACE
+  log_write("device read operation:\nname:%s  addr:%u  offset:%u  len:%d  val:%u\n", map->name,
+		 map->low,  offset, len, ret);
+#endif
   return ret;
 }
 
@@ -66,5 +70,9 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+#ifdef CONFIG_DTRACE
+  log_write("device write operation:\nname:%s  addr:%u offset:%u  len:%d  val:%u\n", mao->name,
+		  map->low, offset, len, data);
+#endif
   invoke_callback(map->callback, offset, len, true);
 }
