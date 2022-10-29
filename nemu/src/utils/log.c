0f46cf362d2bc9ce39befb111392f8e054b08592
tracer-ics2022 <tracer@njuics.org>
2022-10-29 21:44:56 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:44:56 up  1:36,  1 user,  load average: 0.24, 0.23, 0.24

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -29,7 +29,7 @@ void init_log(const char *log_file) {
 }
 
 bool log_enable() {
-    printf("%llu\n", g_nr_guest_inst);
+    printf("%lu\n", g_nr_guest_inst);
    	return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
          (g_nr_guest_inst <= CONFIG_TRACE_END), false);
 }
