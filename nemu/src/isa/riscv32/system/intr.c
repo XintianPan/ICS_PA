5a8bcd71120a1c16175f023e34be57df45941ec3
tracer-ics2022 <tracer@njuics.org>
2022-10-29 21:18:28 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:18:28 up  1:10,  1 user,  load average: 0.31, 0.29, 0.28

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,7 +20,7 @@
   bool etr_en = false;
 #endif
 
-word_t isa_raise_in tr(word_t NO, vaddr_t epc) {
+word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger a n  interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
