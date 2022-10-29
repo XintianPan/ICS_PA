a1b2797f2478a947327f9386f55ae61eb44ace5f
tracer-ics2022 <tracer@njuics.org>
2022-10-29 20:29:16 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:29:16 up 20 min,  1 user,  load average: 0.25, 0.30, 0.23

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -247,6 +247,7 @@ static void statistic() {
 	destruct();
   #endif
   #ifdef CONFIG_ETRACE
+	puts("real");
     for(int i = 0; i < id; ++i) log_write("%s", etr_buf[i]);
   #endif
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
