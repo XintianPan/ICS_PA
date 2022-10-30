ca33b8a8c48186ce0dad88b53cb64f9cfdb7e68d
tracer-ics2022 <tracer@njuics.org>
2022-10-30 22:45:33 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:45:33 up 55 min,  1 user,  load average: 0.33, 0.28, 0.29

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,14 @@
 #include <common.h>
 #include "syscall.h"
 void do_syscall(Context *c) {
-  uintptr_t a[4];
+  uintptr_t a[5];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   a[4] = c->GPRx;
   switch (a[0]) {
+	case SYS_exit:  halt(a[1]); break;
 	case SYS_yield: yield(); c->mepc += 4; a[4] = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
