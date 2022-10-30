334b7ebbc110248d5a55335b80a2babf3140ef1d
tracer-ics2022 <tracer@njuics.org>
2022-10-30 19:32:20 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:32:20 up 3 min,  1 user,  load average: 0.35, 0.22, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,6 +22,7 @@
 #endif 
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
+void _start();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
 	Elf_Ehdr elf_ehdr;
@@ -52,7 +53,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 			for(; j < mem; ++j) *(volatile uint8_t *)(vaddr + j) = 0;	
 		}
 	}
-	return 0;
+	return (uintptr_t)(_start);
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
