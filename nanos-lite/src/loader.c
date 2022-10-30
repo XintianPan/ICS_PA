6670c37301641e67dae3c078599d9863c958e3e3
tracer-ics2022 <tracer@njuics.org>
2022-10-30 17:25:11 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:25:11 up  1:41,  1 user,  load average: 0.58, 0.37, 0.30

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -26,19 +26,30 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
 	Elf_Ehdr elf_ehdr;
 	Elf_Phdr elf_phdr;
-	size_t byte;
-	byte = ramdisk_read(&elf_ehdr, 0, sizeof(Elf_Ehdr));
-	assert(byte == sizeof(Elf_Ehdr));
+	size_t bytes;
+	bytes = ramdisk_read(&elf_ehdr, 0, sizeof(Elf_Ehdr));
+	assert(bytes == sizeof(Elf_Ehdr));
 	assert(*(uint32_t *)elf_ehdr.e_ident = 0x7f454c46);
 	assert(elf_ehdr.e_machine == EXPECT_TYPE);
     size_t i = 0;
-//	size_t file, mem;
+	size_t j = 0;
+	size_t file, mem;
+	uint8_t byte;
+	uint32_t vaddr;
 	uint32_t off = elf_ehdr.e_phoff;
 	for(; i < elf_ehdr .e_phnum; ++i){
-		byte = ramdisk_read(&elf_phdr, off + i * (sizeof(Elf_Phdr)), sizeof(Elf_Phdr));
-	    assert(byte == sizeof(Elf_Phdr));
+		bytes = ramdisk_read(&elf_phdr, off + i * (sizeof(Elf_Phdr)), sizeof(Elf_Phdr));
+	    assert(bytes == sizeof(Elf_Phdr));
 	    if(elf_phdr.p_type == PT_LOAD){
-			
+			vaddr = elf_phdr.p_vaddr;
+		    file = elf_phdr.p_filesz;
+			mem = elf_phdr.p_memsz;
+			j = 0;
+			for(; j < file; ++j){
+				ramdisk_read(&byte, elf_phdr.p_offset + j, 1);
+				*(volatile uint8_t *)(vaddr + j) = byte;
+			}
+			for(; j < mem; ++j) *(volatile uint8_t *)(vaddr + j) = 0;	
 		}
 	}
 	return 0;
