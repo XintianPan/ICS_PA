7a47e0ae1b9e72650d0c0bb73748fedf5c977eb7
tracer-ics2022 <tracer@njuics.org>
2022-11-06 14:37:03 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:37:03 up 15 min,  1 user,  load average: 0.22, 0.19, 0.15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,9 +16,26 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4; //a2 used for argument
   a[4] = c->GPRx; //a0 used for return
   switch (a[0]) {
-	case SYS_exit: Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[1]);  halt(a[1]); break;
-	case SYS_yield: Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[1]);yield(); c->mepc += 4; a[4] = 0; break;
-	case SYS_write:  Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[3]); if(a[1] == 1 || a[1] == 2) mywrite((char *)a[2], a[3]);  a[4] = a[3]; c->mepc += 4;  break;
+	case SYS_exit:
+	   	Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[1]);
+	   	halt(a[1]);
+	   	break;
+	case SYS_yield: 
+		Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[1]);
+		yield(); 
+		c->mepc += 4; a[4] = 0; break;
+	case SYS_write: 
+	   	Log("syscall:%s 1st arg:%d 2nd arg:%p 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[3]); 
+		if(a[1] == 1 || a[1] == 2)
+		   	mywrite((char *)a[2], a[3]); 
+	   	a[4] = a[3];
+	   	c->mepc += 4;  
+		break;
+	case SYS_brk:
+		Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], 0);
+		a[4] = 0;
+		c->mepc += 4;	
+		break;
 	default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
