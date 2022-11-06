51239a276faa00279e711a55fa23e487968dad85
tracer-ics2022 <tracer@njuics.org>
2022-11-06 14:44:04 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:44:04 up 22 min,  1 user,  load average: 0.35, 0.41, 0.28

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include "syscall.h"
 
-extern void *end;
+extern end;
 // helper macros
 #define _concat(x, y) x ## y
 #define concat(x, y) _concat(x, y)
@@ -68,8 +68,8 @@ int _write(int fd, void *buf, size_t count) {
 void *_sbrk(intptr_t increment) {
 	intptr_t ret = _syscall_(SYS_brk, increment, 0, 0);
 	if(ret == 0){
-		void *old = (void *)end;
-		end = end + increment;
+		void *old = (void *)&end;
+		&end = &end + increment;
 		return old;
 	}else{
 		return (void *)-1;
