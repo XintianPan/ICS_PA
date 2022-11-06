2361d6f97c39609d582528bcf0c767a02b6dfbbb
tracer-ics2022 <tracer@njuics.org>
2022-11-06 17:03:49 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:03:49 up  2:41,  1 user,  load average: 0.40, 0.43, 0.37

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,8 +4,9 @@
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
+  int count = 10;
   volatile int j = 0;
-  while (1) {
+  while (count--) {
     j ++;
     if (j == 10000) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
