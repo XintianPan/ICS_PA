192ce2f4652ffdc2be6ee5882a115337cb80fc29
tracer-ics2022 <tracer@njuics.org>
2022-11-06 17:31:36 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:31:36 up  3:09,  1 user,  load average: 0.67, 0.52, 0.38

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -69,7 +69,7 @@ int  _write(int fd, void *buf, size_t count) {
 
 void  *_sbrk(intptr_t increment) {
 	volatile intptr_t temp = cur_addr;
-	volatile rec = _syscall_(SYS_brk, increment, (intptr_t)(&cur_addr), 0);
+	int rec = (int)_syscall_(SYS_brk, increment, (intptr_t)(&cur_addr), 0);
 	if(rec == 0){
 		void *old = (void *)temp;
 		return old;
