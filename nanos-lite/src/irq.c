26a8bfd862b15d2dce948238289a51c4940ca273
tracer-ics2022 <tracer@njuics.org>
2022-11-07 12:44:12 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:44:12 up  1:01,  1 user,  load average: 0.44, 0.43, 0.38

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,8 +4,14 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
    switch (e.event) {
-	case EVENT_YIELD: printf("catch this!\n"); c->mepc += 4; break;
-	case EVENT_SYSCALL: printf("got this!\n"); do_syscall(c); break; 
+	case EVENT_YIELD: 
+//		printf("catch this!\n");
+	   	c->mepc += 4;
+	   	break;
+	case EVENT_SYSCALL: 
+//		printf("got this!\n");
+	   	do_syscall(c);
+	   	break; 
     default: panic("Unhandled event ID = %d", e.event);
   }
 
