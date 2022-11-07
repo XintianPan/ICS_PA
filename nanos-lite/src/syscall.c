b23f502b689f0f5e2334db7e935afc84c885b160
tracer-ics2022 <tracer@njuics.org>
2022-11-07 12:17:40 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:17:40 up 35 min,  1 user,  load average: 0.27, 0.36, 0.35

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,11 +8,44 @@
  * but if not being casted, it's simply treated as value!!!
  * */
 
+char *syscall_name[] = {
+  "exit",
+  "yield",
+  "open",
+  "read",
+  "write",
+  "kill",
+  "getpid",
+  "close",
+  "lseek",
+  "brk",
+  "fstat",
+  "time",
+  "signal",
+  "execve",
+  "fork",
+  "link",
+  "unlink",
+  "wait",
+  "times",
+  "gettimeofday"
+};
+
+int fs_open(const char *pathname, int flags, int mode);
+
+int fs_close(int fd);
+
+size_t fs_read(int fd, void *buf, size_t len);
+
+size_t fs_write(int fd, const void *buf, size_t len);
+
+size_t fs_lseek(int fd, size_t offset, int whence);
+
 void mywrite(char *buf, uint32_t count){
 	uint32_t i = 0;
 	for(; i < count; ++i){
 		putch(*(buf + i));
-	}
+	} 
 }
 
 void do_syscall(Context *c) {
@@ -24,22 +57,46 @@ void do_syscall(Context *c) {
   a[4] = c->GPRx; //a0 used for return
    switch (a[0]) {
 	case SYS_exit:
-	   	Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[1]);
+	   	Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d", syscall_name[a[0]], a[1], a[2], a[3]);
 	   	halt(a[1]);
 	   	break;
 	case SYS_yield: 
-		Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[1]);
+		Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d", syscall_name[a[0]], a[1], a[2], a[3]);
 		yield(); 
 		c->mepc += 4; c->gpr[10] = 0; break;
+	case SYS_open:
+		Log("syscall:%s 1st arg:%s 2nd arg:%d 3rd arg:%d", syscall_name[a[0]],(char *)a[1], a[2], a[3]);
+		c->gpr[10] = fs_open((char *)a[1], (int)a[2], (int)a[3]);
+		c->mepc += 4;
+		break;
+	case SYS_read:
+		Log("syscall:%s 1st arg:%d 2nd arg:%p 3rd arg:%d", syscall_name[a[0]], a[1], a[2], a[3]);
+		c->gpr[10] = fs_read((int)a[1], (void *)a[2], a[3]);
+		c->mepc += 4;
+		break;
 	case SYS_write: 
-	   	Log("syscall:%s 1st arg:%d 2nd arg:%p 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], a[3]); 
-		if(a[1] == 1 || a[1] == 2)
-		   	mywrite((char *)a[2], a[3]); 
-	   	c->gpr[10] = a[3];
+	   	Log("syscall:%s 1st arg:%d 2nd arg:%p 3rd arg:%d", syscall_name[a[0]], a[1], a[2], a[3]); 
+		if(a[1] == 1 || a[1] == 2){
+		   	mywrite((char *)a[2], a[3]);
+			c->gpr[10] = a[3];
+		}else{
+			size_t ret = fs_write(a[1], (void *)a[2], a[3]);
+			c->gpr[10] = ret;
+		}
 	   	c->mepc += 4;  
 		break;
+	case SYS_close:
+		Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d", syscall_name[a[0]], a[1], a[2], a[3]);
+		c->gpr[10] = fs_close(a[1]);
+		c->mepc += 4;
+		break;
+	case SYS_lseek:
+		Log("syscall:%s 1st arg:%d 2nd arg:%d 3rd arg:%d", syscall_name[a[0]], a[1], a[2], a[3]);
+		c->gpr[10] = fs_lseek(a[1], a[2], a[3]);
+		c->mepc += 4;
+		break;
 	case SYS_brk:
-		Log("syscall:%s 1st arg:%d 2nd arg:%p 3rd arg:%d ret val:%d", syscall_name[a[0]], a[1], a[2], a[3], 0);
+		Log("syscall:%s 1st arg:%d 2nd arg:%p 3rd arg:%d", syscall_name[a[0]], a[1], a[2], a[3]);
 		Log("%x", *(int*)a[2]);
 	    *(int *)a[2] = *(int*)a[2] + a[3];
 		Log("%x", *(int*)a[2]);
