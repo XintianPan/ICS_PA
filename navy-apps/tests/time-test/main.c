6b8dd504c3b9a6a8324d786ee619e3c1b8036bc0
tracer-ics2022 <tracer@njuics.org>
2022-11-07 15:53:36 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:53:36 up  1:54,  1 user,  load average: 0.55, 0.47, 0.36

--- navy-apps/tests/time-test/main.c
+++ navy-apps/tests/time-test/main.c
@@ -6,12 +6,14 @@ int main(){
 	struct timeval tv;
 	long pus, ous;
 	gettimeofday(&tv, NULL);
+	int i = 0;
 	pus = tv.tv_usec;
 	while(1){
 		gettimeofday(&tv, NULL);
 		ous = tv.tv_usec;
 		if((ous - pus) / 1000 >= 500){
-			printf("Get here Man!\n");
+			printf("Get here Man: %d!\n", i);
+			++i;
 			pus = ous;
 		}
 	}
