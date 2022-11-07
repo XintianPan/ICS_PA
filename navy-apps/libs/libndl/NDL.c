215cd6f986bed4598d931302d0bf24609bc17278
tracer-ics2022 <tracer@njuics.org>
2022-11-07 19:08:23 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:08:23 up 54 min,  1 user,  load average: 0.62, 1.10, 1.16

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -58,11 +58,12 @@ int NDL_QueryAudio() {
 }
 
 int NDL_Init(uint32_t flags) {
-  if (getenv("NWM_APP")) {
-    evtdev = 3;
-	printf("Init_here\n");
-  }
-  return 0;
+	printf("reach\n");
+	if (getenv("NWM_APP")) {
+		evtdev = 3;
+		printf("Init_here\n");
+	}
+	return 0;
 }
 
 void NDL_Quit() {
