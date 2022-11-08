b4b94a50d42e28a98d763dbb7c1aa1ba815f04f2
tracer-ics2022 <tracer@njuics.org>
2022-11-09 06:48:26 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:48:26 up 20 min,  1 user,  load average: 0.24, 0.18, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -7,6 +7,8 @@
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+#define AUDIO_READ_ADDR      (AUDIO_ADDR + 0x18)
+#define AUDIO_WRITE_ADDR     (AUDIO_ADDR + 0x1c)
 
 void __am_audio_init() {
 }
@@ -29,12 +31,14 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
 	int sb_size = inl(AUDIO_SBUF_SIZE_ADDR);
 	int len = ctl->buf.end - ctl->buf.start;
+	int w = inl(AUDIO_WRITE_ADDR);
 	while(sb_size - (int)inl(AUDIO_COUNT_ADDR) < len) ;
 	uint8_t *s = (uint8_t *)ctl->buf.start;
 	int i = 0;
-	for(; i < len; ++i){
-		outb(AUDIO_SBUF_ADDR + inl(AUDIO_COUNT_ADDR) + i, *s);
+	for(; i < len; ++i){ 
+		outb(AUDIO_SBUF_ADDR + (w + i) % sb_size, *s);
 		++s;
 	}
+	outl(AUDIO_WRITE_ADDR, w + len);
 	outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len);
 }
