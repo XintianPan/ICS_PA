7b499f9d8c469415675b1956f93595a8c3693917
tracer-ics2022 <tracer@njuics.org>
2022-11-08 14:28:27 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:28:27 up 8 min,  1 user,  load average: 0.72, 0.61, 0.31

--- navy-apps/tests/time-test/main.c
+++ navy-apps/tests/time-test/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <NDL.h>
+#include <time.h>
 
 int main(){
 	uint32_t pms, oms;
 	int i = 0;
+	printf("%u\n", sizeof(struct timeval));
 	NDL_Init(0);
 	pms = NDL_GetTicks();
 	while(1){ 
