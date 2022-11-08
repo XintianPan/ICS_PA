f2cee77f440f2cf461ed0cf4b1f3dfcde6e5abcb
tracer-ics2022 <tracer@njuics.org>
2022-11-08 20:53:12 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:53:12 up 6 min,  1 user,  load average: 0.44, 0.42, 0.22

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,7 +10,7 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-#if defined(__ISA_NATIVE__)
+#if defined(__ISA_AM_NATIVE__)
 #define EXPECT_TYPE EM_X86_64
 #elif defined(__ISA_X86__)
 #define EXPECT_TYPE EM_X86_64
