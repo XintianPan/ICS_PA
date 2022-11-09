781d90788994ea7a55762bc9498a24990c908b4a
tracer-ics2022 <tracer@njuics.org>
2022-11-09 16:40:47 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:40:47 up 42 min,  1 user,  load average: 0.73, 0.35, 0.25

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -127,35 +127,47 @@ typedef	__uint128_t fixedptud;
 
 /* Multiplies a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_muli(fixedpt A, int B) {
-	return 0;
-}
+	return A * B;
+} 
 
 /* Divides a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_divi(fixedpt A, int B) {
-	return 0;
-}
+	return A / B;
+} 
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-	return 0;
-}
+	return ((A / FIXEDPT_ONE) * B);
+} 
 
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-	return 0;
-}
+	return (A / B) * FIXEDPT_ONE;
+} 
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
-	return 0;
-}
+	return A >= 0 ? A : -A;
+} 
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-	return 0;
+	uint32_t frac = A & FIXEDPT_FMASK;
+	if(frac){
+		A = A & FIXEDPT_FMASK;
+		return A - FIXEDPT_ONE; // if the fraction part is not equal to zero, the floor should be A - 1
+	}else{
+		return A; // when the fraction part is zero, just return itself
+	}
 }
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {
-	return 0;
+	uint32_t frac = A & FIXEDPT_FMASK;
+	if(frac){
+		A = A & FIXEDPT_FMASK;
+		return A + FIXEDPT_ONE;
+	}else{
+		return A;
+	}
 }
 
 /*
---
+++ navy-apps/tests/fixfloat-test/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <assert.h>
+#include <fixedptc.h>
+int main(){
+	return 0;
+}
