fdf3edfd0e44a9a6ebad54e60c531414cf38bf85
tracer-ics2022 <tracer@njuics.org>
2022-11-09 17:03:03 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:03:03 up  1:04,  1 user,  load average: 0.30, 0.24, 0.23

--- navy-apps/tests/fixfloat-test/main.c
+++ navy-apps/tests/fixfloat-test/main.c
@@ -4,7 +4,17 @@
 int main(){
 	fixedpt a = fixedpt_rconst(1.5);
 	fixedpt b = fixedpt_rconst(-1.5);
+	fixedpt c = fixedpt_rconst(0);
+	fixedpt d = fixedpt_rconst(1);
+	fixedpt e = fixedpt_rconst(-1);
+	fixedpt f = fixedpt_rconst(100.590234);
+	fixedpt g = fixedpt_rconst(-1002.32423599);
 	assert(fixedpt_floor(a) == fixedpt_rconst(1));
+	assert(fixedpt_floor(c) == fixedpt_rconst(0));
+	assert(fixedpt_floor(d) == fixedpt_rconst(1));
+	assert(fixedpt_floor(e) == fixedpt_rconst(-1));
+	assert(fixedpt_floor(f) == fixedpt_rconst(100));
+	assert(fixedpt_floor(g) == fixedpt_rconst(-1003));
 //	assert(fixedpt_ceil(a) == fixedpt_rconst(2));
 //	assert(fixedpt_ceil(b) == fixedpt_rconst(-1));
 	assert(fixedpt_floor(b) == fixedpt_rconst(-2));
