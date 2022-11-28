4d3ed2d2f30b214c7db300d372cf235d729fadad
tracer-ics2022 <tracer@njuics.org>
2022-11-28 18:58:18 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:58:18 up  4:46,  1 user,  load average: 0.20, 0.32, 0.37

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -192,6 +192,8 @@ static int decode_exec(Decode *s, bool *jmp) {
 }
 
 int isa_exec_once(Decode *s, bool *jmp) {
+  printf("0x%08x ", s->snpc);
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
+  printf("0x%08x\n", s->isa.inst.val);
   return decode_exec(s, jmp);
 }
