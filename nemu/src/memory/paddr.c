6711817ff220ec23d635dac804ba49a6853f2a20
tracer-ics2022 <tracer@njuics.org>
2022-11-28 21:12:27 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:12:27 up  7:01,  1 user,  load average: 0.32, 0.62, 1.09

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -79,7 +79,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if(addr == 0x830767874) printf("write to 0x%08x\n", data);
+  if(addr == (paddr_t)0x83076874) printf("write to 0x%08x\n", data);
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
