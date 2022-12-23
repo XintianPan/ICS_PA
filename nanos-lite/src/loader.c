8ef988436d4d443e4ceb84ba4c3baeffc51ba065
tracer-ics2022 <tracer@njuics.org>
2022-12-23 20:03:50 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:03:50 up  3:32,  1 user,  load average: 0.16, 0.09, 0.15

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -72,10 +72,42 @@ void naive_uload(PCB *pcb, const char *filename) {
   ((void(*)())entry) ();
 }
 
-void context_uload(PCB *pcb, const char *filename){
+void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
   uintptr_t entry = loader(pcb, filename);
   Area kustack;
   kustack.start = (void *)pcb;
   kustack.end = (void *)pcb + sizeof(PCB) - 1;
-  pcb->cp = ucontext(&pcb->as, kustack,(void *)entry);
+  pcb->cp = ucontext(&pcb->as, kustack, (void *)entry);
+  int argc = 0;
+  int envpc = 0;
+  if(argv != NULL){
+	while(argv[argc] != NULL) ++argc;
+  }
+  if(envp != NULL){
+	while(envp[envpc] != NULL) ++envpc;
+  }
+  int *argc_pos = (int *)pcb->cp->GPRx;
+  uintptr_t *arg_env_pos = (uintptr_t *)((void *)pcb->cp->GPRx + sizeof(int));
+  *argc_pos = argc;
+  char *string_area = (char *)((void *)pcb->cp->GPRx + sizeof(int) + argc + envpc + 2);
+  for(int i = 0; i < argc; ++i, ++arg_env_pos){
+    *arg_env_pos = (uintptr_t)string_area;
+	for(char *c = argv[i]; *c != '\0'; ++c, ++string_area){
+	  *string_area = *c;
+	}
+	*string_area = '\0';
+	Log("%s", (char *)(arg_env_pos));
+	++string_area;	
+  }
+  *arg_env_pos = (uintptr_t)NULL;
+  ++arg_env_pos;
+  for(int i = 0; i < envpc; ++i, ++arg_env_pos){
+    *arg_env_pos = (uintptr_t)string_area;
+	for(char *c = envp[i]; *c != '\0'; ++c, ++string_area){
+	  *string_area = *c;
+	}
+	*string_area = '\0';
+	++string_area;	
+  }
+ *arg_env_pos = (uintptr_t)NULL; 
 }
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -10,7 +10,7 @@ void context_kload(PCB *pcb, void(*entry)(void *), void *arg);
 
 void naive_uload(PCB *pcb, const char *filename); 
 
-void context_uload(PCB *pcb, const char *filename);
+void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 
 void switch_boot_pcb() {
   current = &pcb_boot;
@@ -35,8 +35,11 @@ void context_kload(PCB *pcb, void(*entry)(void *), void *arg){
 
 
 void init_proc() {
+  char *parse_arg[2];
+  parse_arg[0] = "--skip";
+  parse_arg[1] = NULL;
   context_kload(&pcb[0], hello_fun, (void *)" Hell this ");
-  context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[1], "/bin/pal", parse_arg, NULL);
   switch_boot_pcb();
 
   Log("Initializing processes...");
