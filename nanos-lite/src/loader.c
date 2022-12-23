ef54acef0cafbc73878bc792eac45505d0022988
tracer-ics2022 <tracer@njuics.org>
2022-12-23 22:37:07 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:37:07 up  6:06,  1 user,  load average: 0.12, 0.24, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -105,9 +105,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   }
   *arg_env_pos = (uintptr_t)envpc;
   --arg_env_pos;
-  Log("envpc:%d", *(arg_env_pos + 1));
+  Log("envpc:%d", *(argc_pos - 2 - argc));
   *arg_env_pos = (uintptr_t)NULL;
   --arg_env_pos;
+  Log("%p", arg_env_pos);
   for(int i = envpc - 1; i >= 0; --i, --arg_env_pos){
     *arg_env_pos = (uintptr_t)string_area;
 	for(char *c = envp[i]; *c != '\0'; ++c, ++string_area){
