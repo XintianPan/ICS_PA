fd61d1a649b4a379ee845c6a80e576291e9369cf
tracer-ics2022 <tracer@njuics.org>
2022-12-23 22:39:44 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:39:44 up  6:08,  1 user,  load average: 0.52, 0.35, 0.29

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,8 +8,8 @@ void call_main(uintptr_t *args) {
   int argc = *(int *)(args);
 //  argc = *argc_addr;
   char **argv = (char **)(args - 1 - argc);
-  int enpvc = *(int *)(args - 2 - argc);
-  char **envp = (char **)(args - 3 - argc - enpvc);
+  int envpc = *(int *)((int *)args - 2 - argc);
+  char **envp = (char **)((int *)args - 3 - argc - envpc);
   environ = envp;
   exit(main(argc, argv, envp));
   assert(0);
