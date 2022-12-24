c35d0f93ca1a57499f13c5eee64b5ec9014b1751
tracer-ics2022 <tracer@njuics.org>
2022-12-24 11:36:12 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:36:12 up  1:33,  1 user,  load average: 0.20, 0.24, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -98,7 +98,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   char *string_area = (char *)((void *)pcb->cp->GPRx - PGSIZE);
   *arg_env_pos = (uintptr_t)NULL;
   --arg_env_pos;
-  for(int i = argc - 1; i >= 0; --i, --arg_env_pos){ 
+  for(int i = argc - 1; i >= 0 && argc > 0; --i, --arg_env_pos){ 
     *arg_env_pos = (uintptr_t)string_area;
  	for(char *c = argv[i ]; *c != '\0'; ++c, ++string_area){
 	  *string_area = *c;
@@ -112,13 +112,13 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   --arg_env_pos;
   *arg_env_pos = (uintptr_t)NULL;
   --arg_env_pos;
-  for(int i = envpc - 1; i >= 0; --i, --arg_env_pos){
+  for(int i = envpc - 1; i >= 0 && envpc > 0; --i, --arg_env_pos){
     *arg_env_pos = (uintptr_t)string_area;
 	for(char *c = envp[i]; *c != '\0'; ++c, ++string_area){
 	  *string_area = *c;
-	}
+	 }
 	*string_area = '\0';
 	++string_area;	
-  }
+  } 
   *arg_env_pos = (uintptr_t)NULL;
 }
