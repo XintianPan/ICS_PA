f1363f6885609fc11886e0d4cad71f8e61d3a55c
tracer-ics2022 <tracer@njuics.org>
2022-12-24 12:46:04 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:46:04 up 2 min,  1 user,  load average: 0.26, 0.23, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -119,7 +119,12 @@ void do_syscall(Context *c) {
 //			Log("%s", (char *)a[1]);
 //			Log("%p %p", a[2], a[3]);
 			context_uload(current, (char *)a[1], (char **)a[2], (char **)a[3]);
-			Log("%p", c->mepc);
+			for(int i = 0; i < 32; ++i){
+				c->gpr[i] = current->cp->gpr[i];
+			}
+			c->mstatus = current->cp->mstatus;
+			c->mcause = current->cp->mcause;
+			c->mepc = current->cp->mepc;
 		}
 		else
 			c->gpr[10] = -1, c->mepc += 4;	
