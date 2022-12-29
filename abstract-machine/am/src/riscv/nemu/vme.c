b0dce88214f219a81c4da08b61c8094c6291d05a
tracer-ics2022 <tracer@njuics.org>
2022-12-29 11:06:37 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:06:37 up 29 min,  1 user,  load average: 0.27, 0.17, 0.11

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -31,7 +31,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   pgfree_usr = pgfree_f;
 
   kas.ptr = pgalloc_f(PGSIZE);
-
+  printf("dir:%p\n", kas.ptr);
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
@@ -75,12 +75,15 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 	uintptr_t vpn1 = VPN(va_addr, 31, 22);
 	uintptr_t vpn0 = VPN(va_addr, 21, 12);
 	PTE *fir_dir = dir_addr + vpn1;
+	printf("addr:%p 0x%x\n", fir_dir, vpn1);
 	if((*fir_dir & 1) == 1)
 		return ;
 	void* new_pg = pgalloc_usr(PGSIZE);
 	uintptr_t ppn_fir = (uintptr_t)new_pg >> 12;
+	printf("ppn_sec: 0x%x\n", ppn_fir);
 	*fir_dir = (ppn_fir << 10) | PTE_V;
     PTE *sec_dir = (uintptr_t *)new_pg + vpn0;
+	printf("sec_pos:%p\n", sec_dir);
 	uintptr_t final_ppn = pa_addr >> 12;
 	*sec_dir = (final_ppn << 10) | PTE_R | PTE_W | PTE_X | PTE_V;	
 }
--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -23,12 +23,15 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 	vaddr_t vpn0 = BITS(vaddr, 21, 12);
 	paddr_t dir = BITS(cpu.sys[4], 19, 0);
 	dir <<= 12;
+	printf("dir: 0x%x\n", dir);
 	paddr_t pte = paddr_read(dir + vpn1 * 4, 4);
 	assert((pte & 1) == 1);
 	paddr_t ppn = pte >> 10;
 	ppn <<= 12;
+	printf("ppn: 0x%x\n", ppn);
 	paddr_t pte0 = paddr_read(ppn + vpn0 * 4, 4);
 	paddr_t pg = pte0 >> 10;
+	printf("final page: 0x%x\n", (pg << 12));
 	paddr_t paddr = (pg << 12) | pg_off;
 	return paddr;
 }
