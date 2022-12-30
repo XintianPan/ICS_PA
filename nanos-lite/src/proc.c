4149977ee1854b2be3adbe7387af787348cc4b7b
tracer-ics2022 <tracer@njuics.org>
2022-12-30 22:03:19 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:03:19 up  1:33,  1 user,  load average: 0.51, 0.48, 0.51

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -43,7 +43,7 @@ static char *parse_envp[] = {NULL };
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)" Hell this ");
-  context_uload(&pcb[1], "/bin/dummy", parse_arg, parse_envp);
+  context_uload(&pcb[1], "/bin/hello", parse_arg, parse_envp);
   switch_boot_pcb();
 
   Log("Initializing processes...");
