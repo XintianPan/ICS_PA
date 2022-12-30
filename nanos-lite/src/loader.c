9b4241da57b8faa7ba91f6cadd09764601ca0c0b
tracer-ics2022 <tracer@njuics.org>
2022-12-30 10:38:34 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:38:34 up 53 min,  1 user,  load average: 0.10, 0.09, 0.14

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -70,6 +70,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 			size_t fremain = file % PGSIZE;
 			for(; j <= pg_count ; ++j){
 				pa = new_page(1);
+				Log("run this");
 				map(&pcb->as, (void *)vaddr, pa, 0);
 				if(j <= fpg_count){
 					fs_read(fd, page_cache, PGSIZE);
