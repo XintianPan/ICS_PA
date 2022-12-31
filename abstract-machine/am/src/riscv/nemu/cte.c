e27a16870736f8f6ec92a1b7c3cbe55427af2d64
tracer-ics2022 <tracer@njuics.org>
2022-12-31 16:33:56 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:56 up  6:46,  1 user,  load average: 0.24, 0.17, 0.21

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -58,7 +58,7 @@ Context* __am_irq_handle(Context *c) {
 //  printf("%x\n", c->mepc);
   printf("here %x\n", c->gpr[2]);
   uintptr_t pre_sp = get_ms();
-  printf("os %x\n", pre_sp);
+  printf("os %x %x\n", pre_sp, sizeof(Context));
   if(pre_sp - sizeof(Context) != c->gpr[2]){
 	c->gpr[0] = 1;
   }else{
