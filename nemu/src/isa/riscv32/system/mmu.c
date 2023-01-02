8a8b536575a6447ab1ed1848bdc1f356c947c25f
tracer-ics2022 <tracer@njuics.org>
2023-01-02 15:28:43 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:28:43 up  5:05,  1 user,  load average: 0.39, 0.19, 0.11

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -26,6 +26,7 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 //	printf("dir: 0x%x\n", dir);
 	paddr_t pte = paddr_read(dir + vpn1 * 4, 4);
 	if((pte & 1) != 1){
+		printf("0x%08x\n", cpu.sys[5]);
 		printf("0x%08x\n", vaddr);
 		assert(0);
 	}
