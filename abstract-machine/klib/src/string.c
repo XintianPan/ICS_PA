115cb082418632a297a0daaaf2781fe912d6232f
tracer-ics2022 <tracer@njuics.org>
2023-01-03 18:02:13 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:02:13 up  7:27,  1 user,  load average: 0.26, 0.20, 0.12

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -84,6 +84,7 @@ void *memcpy(void *out, const void *in, size_t n) {
 	char *dst = (char *)out;
 	const char *src = (const char *)in;
     size_t i;
+	printf("what\n");
 	printf("%p %p\n", out, in);
 	for(i = 0; i < n; ++i)
 		dst[i] = src[i];
