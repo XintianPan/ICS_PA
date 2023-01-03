688b8c252f1692cd1192c1e278bd0ba3c27cb55a
tracer-ics2022 <tracer@njuics.org>
2023-01-03 20:08:35 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:08:35 up  9:33,  1 user,  load average: 0.57, 0.59, 0.45

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -67,6 +67,6 @@ Context* schedule(Context *prev) {
 	current = &pcb[0], time_seg = 0;	
  //   Log("%p %p", pcb[0].cp, pcb[1].cp);
 //	Log("%p %p %p %p %p %p", pcb[0].cp, pcb[1].cp, pcb[0].cp->gpr[10], pcb[1].cp->gpr[10], pcb[0].cp->mepc, pcb[1].cp->mepc);
-    current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
+//    current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
 	return current->cp;
 }
