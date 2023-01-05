3f7fe7b40ef8a7d949ce02a0bb7cadc10a111728
tracer-ics2022 <tracer@njuics.org>
2023-01-05 11:44:02 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:44:02 up  1:13,  1 user,  load average: 0.24, 0.22, 0.16

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -69,7 +69,7 @@ void __am_get_cur_as(Context *c) {
 
 void __am_switch(Context *c) {
   if (vme_enable && c->pdir != NULL) {
-	//  printf("addr:%p\n", c->pdir);
+	  printf("addr:%p\n", c->pdir);
 	  set_satp(c->pdir);
   }
 }
