41e9a7abc784043ca48b00451086528abfad5b8c
tracer-ics2022 <tracer@njuics.org>
2023-01-11 15:07:52 +0800

    >  compile NEMU 211240001 潘昕田 Linux paxintic-ThinkPad-P1-Gen-4i 5.15.0-25-generic #25-Ubuntu SMP Wed Mar 30 15:54:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:07:52 up 11 min,  1 user,  load average: 0.10, 0.44, 0.31

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -53,8 +53,8 @@ void init_proc() {
 //  Log("%p", &pcb[0].cp);
 //  Log("%p", pcb[0].cp);
     context_uload(&pcb[1], "/bin/nterm", NULL, NULL);
-	context_uload(&pcb[2], "/bin/bird", NULL, NULL);
-	context_uload(&pcb[3], "/bin/pal", NULL, NULL);
+	context_uload(&pcb[2], "/bin/menu", NULL, NULL);
+	context_uload(&pcb[3], "/bin/menu", NULL, NULL);
 //  Log("%p", pcb[0].cp);
     pcbid = 1;
   switch_boot_pcb();
